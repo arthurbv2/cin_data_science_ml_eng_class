206480bfd39205e36e63a1e4b569a1463c348aa7
Arthur Caique <arthurcaiquebv2@gmail.com>
2020-10-21 03:28:13 -0300

    mlflow logging

--- dvc_demo/nodes/training_node.py
+++ dvc_demo/nodes/training_node.py
@@ -1,7 +1,12 @@
+from pickle import dump
+from pathlib import Path
+from datetime import datetime
+
 from numpy import where, mean, median, max, min
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import ndcg_score, mean_squared_error
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from mlflow import create_experiment, start_run, log_metrics, log_artifact
 
 
 class TrainingNode:
@@ -34,31 +39,50 @@ class TrainingNode:
 
         return {
             f'{dataset_name}_mse_eval': mse,
-            f'{dataset_name}_ndcg@8_mean': mean(ndcgs),
-            f'{dataset_name}_ndcg@8_median': median(ndcgs),
-            f'{dataset_name}_ndcg@8_max': max(ndcgs),
-            f'{dataset_name}_ndcg@8_min': min(ndcgs)
+            f'{dataset_name}_ndcg_8_mean': mean(ndcgs),
+            f'{dataset_name}_ndcg_8_median': median(ndcgs),
+            f'{dataset_name}_ndcg_8_max': max(ndcgs),
+            f'{dataset_name}_ndcg_8_min': min(ndcgs)
         }
 
     def execute(self):
-        model = self.select_algorithm()
-        model.fit(self.X_train, self.y_train)
-        train_evaluation = self.evaluate(
-            model=model,
-            X=self.X_train,
-            y_true=self.y_train,
-            qids=self.qid_train
-        )
-        test_evaluation = self.evaluate(
-            model=model,
-            X=self.X_test,
-            y_true=self.y_test,
-            qids=self.qid_test
-        )
-        return {
-            **train_evaluation,
-            **test_evaluation
-        }
+
+        start_date = datetime.now().strftime('%H_%M_%S')
+
+        Path(f"database/models/{start_date}").mkdir(parents=True, exist_ok=True)
+
+        experiment_id = create_experiment(name=start_date)
+
+        with start_run(experiment_id=experiment_id):
+
+            model = self.select_algorithm()
+            model.fit(self.X_train, self.y_train)
+            train_evaluation = self.evaluate(
+                model=model,
+                X=self.X_train,
+                y_true=self.y_train,
+                qids=self.qid_train,
+                dataset_name='train'
+            )
+            test_evaluation = self.evaluate(
+                model=model,
+                X=self.X_test,
+                y_true=self.y_test,
+                qids=self.qid_test,
+                dataset_name='test'
+            )
+
+            all_metrics = {
+                **train_evaluation,
+                **test_evaluation
+            }
+
+            dump(obj=model, file=open(f'database/models/{start_date}.pkl', 'wb'))
+
+            log_metrics(metrics=all_metrics)
+            log_artifact(f'database/models/{start_date}.pkl')
+
+            return all_metrics
 
 
 
